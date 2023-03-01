a65c37e6d3bb83738ab2d3a1e4bdb909ebc7fe03
Christoffer <cgranzell@hotmail.com>
2023-03-01 09:57:04 +0100

    spinner upComingEvents

--- components/upComingEvents/UpComingEvents.js
+++ components/upComingEvents/UpComingEvents.js
@@ -4,10 +4,14 @@ import styles from '../../styles/upComingEvents/UpComingEvents.module.css';
 import EventCard from './EventCard';
 import { db } from '@/firebase';
 import { collection, getDocs } from 'firebase/firestore';
+import LoadingSpinner from '../LoadingSpinner';
 
 const UpComingEvents = forwardRef(({ onBackClick }, ref) => {
   // Alla Events
   const [events, setEvents] = useState([]);
+
+  // Spinner
+  const [isLoading, setIsLoading] = useState(false);
 
   // Referens till databasen
   const eventsCollectionRef = collection(db, 'events');
@@ -15,11 +19,13 @@ const UpComingEvents = forwardRef(({ onBackClick }, ref) => {
   // Hämtar events
   useEffect(() => {
     const getEvents = async () => {
+      setIsLoading(true);
       const data = await getDocs(eventsCollectionRef);
       console.log(data);
       setEvents(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getEvents();
+    setIsLoading(false);
   }, []);
 
   // Sorterar events efter datum
@@ -34,11 +40,18 @@ const UpComingEvents = forwardRef(({ onBackClick }, ref) => {
       <div className={styles.headerContainer}>
         <h1>Kommande Events</h1>
       </div>
+
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+
+
       <div className={styles.cardContainer}>
         {sortedEvents.slice(0, 6).map((event) => {
           return <EventCard key={event.id} event={event} />;
         })}
       </div>
+      )}
     </div>
   );
 });
