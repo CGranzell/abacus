aa3f1011a862d26fe0d6eb424efee6a06b1845af
Christoffer <cgranzell@hotmail.com>
2023-03-03 10:15:16 +0100

    welcomeTextCard ny design

--- components/welcomeSection/WelcomeTextCard.js
+++ components/welcomeSection/WelcomeTextCard.js
@@ -14,17 +14,17 @@ const WelcomeTextCard = ( { onBackClick }) => {
 
         <p>
           För speciellt intresserade och särskilt begåvade elever inom
-          matematik, teknik och naturvetenskap finns idag alltför få utmaningar.
+          matematik, teknik och naturvetenskap finns idag alltför få utmaningar. <br/>
           Elever med särskild begåvning behöver bli stimulerade och få möta
           likasinnade elever för att inte tappa motivationen eller hamna i
-          utanförskap. Forskning visar att många talangfulla elever
-          underpresterar för att passa in i den svenska skolan (Sims 2021). Vi
+          utanförskap. <br/> Forskning visar att många talangfulla elever
+          underpresterar för att passa in i den svenska skolan (Sims 2021). <br/> Vi
           vill bemöta behoven hos dessa elever som i brist på motivation och
           utmaning riskerar att bli s.k. hemmasittare eller att drabbas av
           psykisk ohälsa.
         </p>
       <div className={styles.ctaContainer}>
-      <button onClick={onBackClick}>Se våra events</button>
+      <button onClick={onBackClick}>Se alla events {">"} </button>
       </div>
       </div>
       </div>
