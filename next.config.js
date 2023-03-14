53b7aacf502b27e2eb520c3e8e6f3e289c53cf0f
Christoffer <cgranzell@hotmail.com>
2023-03-14 13:32:40 +0100

    config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
- 
+  env: {
+    apiKey: 'AIzaSyCKxxcBNdf_2OEMMquJ5LYnNQAYtkYg6qw',
+    authDomain: 'abacus-camp.firebaseapp.com',
+    projectId: 'abacus-camp',
+    storageBucket: 'abacus-camp.appspot.com',
+    messagingSenderId: '19598447285',
+    appId: '1:19598447285:web:d4c9de7956317df4aae4fc',
+    measurementId: 'G-2S5FFW8RT9',
+  },
 };
 
 module.exports = nextConfig;
