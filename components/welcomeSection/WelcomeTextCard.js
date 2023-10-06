3447faaaf166d29e43b286c21170c25ccf8e210f
cgranzell <cgranzell@hotmail.com>
2023-10-06 13:08:52 +0200

    ändring welcome text

--- components/welcomeSection/WelcomeTextCard.js
+++ components/welcomeSection/WelcomeTextCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from '../../styles/welcomeSection/WelcomeTextCard.module.css';
+import React from "react";
+import styles from "../../styles/welcomeSection/WelcomeTextCard.module.css";
 
 const WelcomeTextCard = ({ onBackClick }) => {
   return (
@@ -7,27 +7,23 @@ const WelcomeTextCard = ({ onBackClick }) => {
       <div className={styles.headerContainer}>
         <h1>Välkommen till AbacusCamp </h1>
         <p>
-          {' '}
-          Avancerad matematik och <br /> programmering{' '}
+        En förening för särbegåvade individer med intresse för matematik, teknik och naturvetenskap.
         </p>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.textWrapper}>
           <p>
-            För speciellt intresserade och särskilt begåvade elever inom <br />
-            matematik, teknik och naturvetenskap finns idag alltför få <br />
-            utmaningar. Elever med särskild begåvning behöver bli stimulerade
-            och få möta likasinnade elever för att inte tappa motivationen{' '}
-            <br /> eller hamna iutanförskap. Forskning visar att många
-            talangfulla <br />
-            elever underpresterar för att passa in i den svenska skolan (Sims
-            2021). Vi vill bemöta behoven hos dessa elever som i brist på
-            <br /> motivation och utmaning riskerar att bli s.k. hemmasittare{' '}
-            <br />
-            eller att drabbas av psykisk ohälsa.
+            AbacusCamp är en förening där individer träffas för att ha roligt,
+            utvecklas och utforska sitt intresse för matematik, teknik och
+            naturvetenskap. Genom att omvandla våra teoretiska beräkningar
+            skapar vi en kreativ verklighet som verifierar teorin. Föreningens
+            huvudsakliga målgrupp är särbegåvade ungdomar som går på högstadiet
+            och i gymnasiet. Föreningen drivs av engagerade coacher som
+            tillsammans med deltagarna träffas fysiskt för det är i dessa möten
+            som magi uppstår.
           </p>
           <div className={styles.ctaContainer}>
-            <button onClick={onBackClick}>Se alla events {'>'} </button>
+            <button onClick={onBackClick}>Se alla events {">"} </button>
           </div>
         </div>
       </div>
