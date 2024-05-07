89bdca7465fee33427f84f907cdbdb6c7d69b4f0
Christoffer Granzell <christoffer.granzell@sogeti.com>
2024-05-07 07:26:12 +0200

    typo

--- firebase.js
+++ firebase.js
@@ -13,7 +13,7 @@ projectId: `${process.env.PROJECT_ID}`,
 storageBucket: `${process.env.STORAGE_BUCKET}`,
 messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
 appId: `${process.env.APP_ID}`,
-measurementId: `${process.env.MEASURMENT_ID}`
+measurementId: `${process.env.MEASUREMENT_ID}`
 }
 
 // Initialize Firebase
