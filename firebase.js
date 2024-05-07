485ffe706daabe20ec3f8c2c9a28112af87e844f
Christoffer Granzell <christoffer.granzell@sogeti.com>
2024-05-07 07:31:30 +0200

    test

--- firebase.js
+++ firebase.js
@@ -5,16 +5,16 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-
-apiKey: `${process.env.API_KEY}`,
-authDomain: `${process.env.AUTH_DOMAIN}`,
-databaseUrl: `${process.env.DATABASE_URL}`,
-projectId: `${process.env.PROJECT_ID}`,
-storageBucket: `${process.env.STORAGE_BUCKET}`,
-messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-appId: `${process.env.APP_ID}`,
-measurementId: `${process.env.MEASUREMENT_ID}`
-}
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
