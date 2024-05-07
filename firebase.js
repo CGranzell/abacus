a780e2cecc1ef2e632ab8df148befae2285988d2
Christoffer Granzell <christoffer.granzell@sogeti.com>
2024-05-07 08:13:55 +0200

    hard code

--- firebase.js
+++ firebase.js
@@ -5,14 +5,24 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    // apiKey: process.env.API_KEY,
+    // authDomain: process.env.AUTH_DOMAIN,
+    // databaseURL: process.env.DATABASE_URL,
+    // projectId: process.env.PROJECT_ID,
+    // storageBucket: process.env.STORAGE_BUCKET,
+    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    // appId: process.env.APP_ID,
+    // measurementId: process.env.MEASUREMENT_ID
+
+    apiKey: "AIzaSyCKxxcBNdf_2OEMMquJ5LYnNQAYtkYg6qw",
+    authDomain: "abacus-camp.firebaseapp.com",
+    databaseURL: "https://abacus-camp-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "abacus-camp",
+    storageBucket: "abacus-camp.appspot.com",
+    messagingSenderId: "19598447285",
+    appId: "1:19598447285:web:d4c9de7956317df4aae4fc",
+    measurementId: "G-2S5FFW8RT9"
+  
   };
   
 
