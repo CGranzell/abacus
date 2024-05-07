c9717f28e2a265fdb0aa9aad45a095ee579ed246
Christoffer Granzell <christoffer.granzell@sogeti.com>
2024-05-07 07:56:55 +0200

    removed keys

--- next.config.js
+++ next.config.js
@@ -4,13 +4,14 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    apiKey: 'AIzaSyCKxxcBNdf_2OEMMquJ5LYnNQAYtkYg6qw',
-    authDomain: 'abacus-camp.firebaseapp.com',
-    projectId: 'abacus-camp',
-    storageBucket: 'abacus-camp.appspot.com',
-    messagingSenderId: '19598447285',
-    appId: '1:19598447285:web:d4c9de7956317df4aae4fc',
-    measurementId: 'G-2S5FFW8RT9',
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   },
 };
 
